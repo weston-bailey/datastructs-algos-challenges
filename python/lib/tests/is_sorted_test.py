2ad29aba4fd5789dbea6ba86d5c4447b5327bddb
ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDFCpkaQgLdANfmSqQ65NlnJ1Q592NjLqG5iHhyENjG9HTPNUSdX8y+sjvKnb7h1B1P2s9jPnlVG3aXb22rK30MG0Y4dCLMwL20FMGc7+4jYH70S8OxetWUgxY7y9/5xdmAJ3AWvuuMNf9s0KMKIfYJl85V8kTAub6JsH/pSlCtT1U56Kt1ao0ipV/o6evBsDV/R1X9gfOspgx53DKuwl//goanRAtqXoEURFW/nt4L8DPfBodtMS+OBtIqR3SobFSd0bChP2PxUD5g08QfYyonX1MZHYWJqEq/T8N/K0N9nfuDjEBm62ELvrK4MWosJuwFLOx+UyzyjIRdsx6NSIf+KIOtWLtKx4ldUWzDhrJyN6m3jNT9JRArh1yO61nZfojLgZ6HKoUYSR85MeuZ4ms+sH7WZL7e7Ht5u/gQi5hBT0cvA4Opky8l4uo7tdi9RUMXZKYHUGIZRzUiQAFRjSMdJ8cUTZmhv6OtLDe/NKEa+m5rBrrdyrVJVPP/C8d/Tlk= wdbailey87@gmail.com <wdbailey87@gmail.com>
2021-09-26 15:24:56 -0700

    update python is sorted

--- python/lib/tests/is_sorted_test.py
+++ python/lib/tests/is_sorted_test.py
@@ -2,10 +2,14 @@ from unittest import TestCase, main
 from ..is_sorted import is_sorted
 
 class TestIsSorted(TestCase):
-  sorted_array = [1, 2, 3, 4, 5, 6, 7, 8]
-  unsorted_array = [2, 1, 4, 3, 6, 7, 5, 7]
+
   def test_sorted_list(self): 
-    self.assertEqual(1, 1)
+    sorted_list = [1, 2, 3, 4, 5, 6, 7, 8]
+    self.assertTrue(is_sorted(sorted_list))
+
+  def test_unsorted_list(self):
+    unsorted_list = [2, 1, 4, 3, 6, 7, 5, 7]
+    self.assertFalse(is_sorted(unsorted_list))
 
 if __name__ == '__main__':
   main()
