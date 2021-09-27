871adb9307e10a700d622b0f91698d25a8b77293
ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDFCpkaQgLdANfmSqQ65NlnJ1Q592NjLqG5iHhyENjG9HTPNUSdX8y+sjvKnb7h1B1P2s9jPnlVG3aXb22rK30MG0Y4dCLMwL20FMGc7+4jYH70S8OxetWUgxY7y9/5xdmAJ3AWvuuMNf9s0KMKIfYJl85V8kTAub6JsH/pSlCtT1U56Kt1ao0ipV/o6evBsDV/R1X9gfOspgx53DKuwl//goanRAtqXoEURFW/nt4L8DPfBodtMS+OBtIqR3SobFSd0bChP2PxUD5g08QfYyonX1MZHYWJqEq/T8N/K0N9nfuDjEBm62ELvrK4MWosJuwFLOx+UyzyjIRdsx6NSIf+KIOtWLtKx4ldUWzDhrJyN6m3jNT9JRArh1yO61nZfojLgZ6HKoUYSR85MeuZ4ms+sH7WZL7e7Ht5u/gQi5hBT0cvA4Opky8l4uo7tdi9RUMXZKYHUGIZRzUiQAFRjSMdJ8cUTZmhv6OtLDe/NKEa+m5rBrrdyrVJVPP/C8d/Tlk= wdbailey87@gmail.com <wdbailey87@gmail.com>
2021-09-27 13:55:41 -0700

    add float_to_str

---
+++ python/lib/tests/data/float_to_str_test.py
@@ -0,0 +1,17 @@
+from unittest import TestCase, main
+from ...data.float_to_str import float_to_str
+
+class Test_Float_To_Strong(TestCase):
+  def test_type(self):
+    fl = 10.1
+    fl_str = float_to_str(fl)
+    self.assertIsInstance(fl_str, str)    
+
+  def test_return(self):
+    sci_note = 1e-15
+    str_repr = "0.000000000000001"
+    return_test = float_to_str(sci_note)
+    self.assertEqual(str_repr, return_test)
+
+if __name__ == '__main__':
+  main()
