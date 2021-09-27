14cfba5bce708cc5bf8ea473298f299467a43bec
ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDFCpkaQgLdANfmSqQ65NlnJ1Q592NjLqG5iHhyENjG9HTPNUSdX8y+sjvKnb7h1B1P2s9jPnlVG3aXb22rK30MG0Y4dCLMwL20FMGc7+4jYH70S8OxetWUgxY7y9/5xdmAJ3AWvuuMNf9s0KMKIfYJl85V8kTAub6JsH/pSlCtT1U56Kt1ao0ipV/o6evBsDV/R1X9gfOspgx53DKuwl//goanRAtqXoEURFW/nt4L8DPfBodtMS+OBtIqR3SobFSd0bChP2PxUD5g08QfYyonX1MZHYWJqEq/T8N/K0N9nfuDjEBm62ELvrK4MWosJuwFLOx+UyzyjIRdsx6NSIf+KIOtWLtKx4ldUWzDhrJyN6m3jNT9JRArh1yO61nZfojLgZ6HKoUYSR85MeuZ4ms+sH7WZL7e7Ht5u/gQi5hBT0cvA4Opky8l4uo7tdi9RUMXZKYHUGIZRzUiQAFRjSMdJ8cUTZmhv6OtLDe/NKEa+m5rBrrdyrVJVPP/C8d/Tlk= wdbailey87@gmail.com <wdbailey87@gmail.com>
2021-09-27 14:15:32 -0700

    add insertion sort

---
+++ python/lib/sort/insertion_sort.py
@@ -0,0 +1,31 @@
+def insertion_sort(li):
+  # iterate through list and move elements down if they are less than the element to the left
+  for index in range(len(li)):
+    # current number for comaprisons
+    current_number = li[index]
+    # use while loop to go back down the list 
+    cur_pos = index - 1
+    while cur_pos >= 0 and current_number < li[cur_pos]:
+      # swap the values
+      li[cur_pos], li[cur_pos + 1] = li[cur_pos + 1], li[cur_pos]
+      # print(f'index: {index} current poition: {cur_pos} comparison: {current_number} < {li[cur_pos]} {li}', end='\r')
+      # time.sleep(.5)
+      cur_pos -= 1
+
+  return li 
+
+# not working
+def insertion_sort_2(unsorted_list):
+
+  for i in range(len(unsorted_list)):
+    sort_recursive(unsorted_list, i)
+  
+  return unsorted_list
+  
+def sort_recursive(unsorted_list, index):
+  # base case
+  if index <= 0:
+    return unsorted_list
+  if unsorted_list[index] < unsorted_list[index - 1]:
+    unsorted_list[index - 1], unsorted_list[index] = unsorted_list[index], unsorted_list[index - 1]
+    return sort_recursive(unsorted_list, index - 1)
--- python/lib/tests/sort/sort_test.py
+++ python/lib/tests/sort/sort_test.py
@@ -1,6 +1,7 @@
 from unittest import TestCase, main
 from ...sort.bubble_sort import *
 from ...sort.is_sorted import *
+from ...sort.insertion_sort import *
 
 class Test_Sorts(TestCase):
 
@@ -23,6 +24,19 @@ class Test_Sorts(TestCase):
   #   sorted_list = bubble_sort_3(unsorted_list)
   #   sorted_test = is_sorted(sorted_list)
   #   self.assertTrue(sorted_test)
+
+  def test_insertion_sort(self):
+    unsorted_list = [2, 1, 4, 3, 6, 7, 5, 7]
+    sorted_list = insertion_sort(unsorted_list)
+    sorted_test = is_sorted(sorted_list)
+    self.assertTrue(sorted_test)
+
+  # def test_insertion_sort_2(self):
+  #   unsorted_list = [2, 1, 4, 3, 6, 7, 5, 7]
+  #   sorted_list = insertion_sort_2(unsorted_list)
+  #   sorted_test = is_sorted(sorted_list)
+  #   self.assertTrue(sorted_test)
+
 
 if __name__ == '__main__':
   main()
