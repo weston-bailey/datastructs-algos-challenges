a8e057a234ca2b9a8d6247d72047980ec1518a49
ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDFCpkaQgLdANfmSqQ65NlnJ1Q592NjLqG5iHhyENjG9HTPNUSdX8y+sjvKnb7h1B1P2s9jPnlVG3aXb22rK30MG0Y4dCLMwL20FMGc7+4jYH70S8OxetWUgxY7y9/5xdmAJ3AWvuuMNf9s0KMKIfYJl85V8kTAub6JsH/pSlCtT1U56Kt1ao0ipV/o6evBsDV/R1X9gfOspgx53DKuwl//goanRAtqXoEURFW/nt4L8DPfBodtMS+OBtIqR3SobFSd0bChP2PxUD5g08QfYyonX1MZHYWJqEq/T8N/K0N9nfuDjEBm62ELvrK4MWosJuwFLOx+UyzyjIRdsx6NSIf+KIOtWLtKx4ldUWzDhrJyN6m3jNT9JRArh1yO61nZfojLgZ6HKoUYSR85MeuZ4ms+sH7WZL7e7Ht5u/gQi5hBT0cvA4Opky8l4uo7tdi9RUMXZKYHUGIZRzUiQAFRjSMdJ8cUTZmhv6OtLDe/NKEa+m5rBrrdyrVJVPP/C8d/Tlk= wdbailey87@gmail.com <wdbailey87@gmail.com>
2021-09-27 14:48:04 -0700

    update sort tests

--- python/lib/tests/sort/sort_test.py
+++ python/lib/tests/sort/sort_test.py
@@ -1,4 +1,5 @@
 from unittest import TestCase, main
+from ...data.random_list import random_list 
 from ...sort.bubble_sort import *
 from ...sort.is_sorted import *
 from ...sort.insertion_sort import *
@@ -6,7 +7,13 @@ from ...sort.insertion_sort import *
 class Test_Sorts(TestCase):
 
   def test_bubble_sort_1(self):
+    # test simple list
     unsorted_list = [2, 1, 4, 3, 6, 7, 5, 7]
+    sorted_list = bubble_sort_1(unsorted_list)
+    sorted_test = is_sorted(sorted_list)
+    self.assertTrue(sorted_test)
+    # test random complex list
+    unsorted_list = random_list(length=500, negative=True)
     sorted_list = bubble_sort_1(unsorted_list)
     sorted_test = is_sorted(sorted_list)
     self.assertTrue(sorted_test)
@@ -26,7 +33,13 @@ class Test_Sorts(TestCase):
   #   self.assertTrue(sorted_test)
 
   def test_insertion_sort(self):
+    # test simplle list
     unsorted_list = [2, 1, 4, 3, 6, 7, 5, 7]
+    sorted_list = insertion_sort(unsorted_list)
+    sorted_test = is_sorted(sorted_list)
+    self.assertTrue(sorted_test)
+    # test random complex list
+    unsorted_list = random_list(length=500, negative=True)
     sorted_list = insertion_sort(unsorted_list)
     sorted_test = is_sorted(sorted_list)
     self.assertTrue(sorted_test)
