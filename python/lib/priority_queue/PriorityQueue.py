40f1537ae3678b643aaa183e6cde34bae73c91bd
weston-bailey <wdbailey87@gmail.com>
2022-05-25 20:31:41 -0700

    adds test

--- python/lib/priority_queue/PriorityQueue.py
+++ python/lib/priority_queue/PriorityQueue.py
@@ -1,3 +1,29 @@
+
+
 class PriorityQueue:
     def __init__(self):
         self.queue = []
+
+    def __str__(self):
+        pass
+    
+    def __repr__(self):
+        pass
+
+    def is_empty(self):
+        '''
+            checks if queue is empty
+        '''
+        pass
+
+    def insert(self, data):
+        '''
+            adds a new data value to queue at last priority
+        '''
+        pass
+
+    def delete(self, data):
+        '''
+            deletes an item from queue based on priority
+        '''
+        pass 
--- python/lib/priority_queue/PriorityQueue_test.py
+++ python/lib/priority_queue/PriorityQueue_test.py
@@ -1,7 +1,13 @@
 from unittest import TestCase, main
+from lib.priority_queue.PriorityQueue import PriorityQueue
 
-class TestPriorityQueue(TestCast):
-    pass
+class TestPriorityQueue(TestCase):
+    def test_is_empty(self):
+        q = PriorityQueue()
+        self.assertFalse(q.is_empty())
+        q.insert(10)
+
+        self.assertTrue(q.is_empty())
 
 if __name__ == '__main__':
     main()
