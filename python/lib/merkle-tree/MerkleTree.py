d9febd8beee8663547d610037ce808d8409e4978
weston-bailey <wdbailey87@gmail.com>
2022-07-09 13:53:25 -0700

    stubs merkle tree

--- python/lib/merkle-tree/MerkleTree.py
+++ python/lib/merkle-tree/MerkleTree.py
@@ -15,15 +15,42 @@ class Node:
         return Node.hash(Node.hash(val))
 
 class MerkleTree:
-    pass
+    def __init__(self, values):
+        self.__build_tree(values)
+        self.root = None
 
+    def __build_tree(self, values):
+        leaves = [Node(Node.double_hash(value)) for value in values]
+        # duplicate last elem if there is an odd number of values
+        if len(leaves) % 2 == 1:
+           # leaves.append(leaves[len(leaves)- 1])
+           leaves.append(leaves[-1:][0])
+        self.root = self.__build_tree_rec(leaves)
+
+        # for leaf in leaves:
+        #     print(leaf.value)
+
+    def __build_tree_rec(self, nodes):
+        pass
+
+    def print_tree(self):
+        pass
+
+    def __print_tree_rec(self):
+        pass
+
+    def get_root_hash(self):
+        pass
 def main():
-    root = Node('root', Node('left'), Node('right'))
-    print(root.value, root.left.value, root.right.value)
-    root.left.value = Node.hash(root.left.value)
-    root.right.value = Node.hash(root.right.value)
-    root.value = Node.hash(root.left.value + root.right.value)
-    print(root.value, root.left.value, root.right.value)
+    # testing the node
+    # root = Node('root', Node('left'), Node('right'))
+    # print(root.value, root.left.value, root.right.value)
+    # root.left.value = Node.hash(root.left.value)
+    # root.right.value = Node.hash(root.right.value)
+    # root.value = Node.hash(root.left.value + root.right.value)
+    # print(root.value, root.left.value, root.right.value)
+    nodes = ['root', 'left', 'right']
+    tree = MerkleTree(nodes)
 
 if __name__ == '__main__':
     main()
