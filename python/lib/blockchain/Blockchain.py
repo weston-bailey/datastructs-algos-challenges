85403cecb08397c06fad1d5356f8269f2962ed92
weston-bailey <wdbailey87@gmail.com>
2022-07-09 21:51:09 -0700

    adds comments to blockchain and removes extra classes

--- python/lib/blockchain/Blockchain.py
+++ python/lib/blockchain/Blockchain.py
@@ -3,31 +3,6 @@ import json
 from time import time
 from pprint import pprint
 
-class Block:
-    def __init__(self, index, timestamp, transactions, proof, previous_hash):
-        self.index = index
-        self.timestamp = timestamp
-        self.transactions = transactions
-        self.proof = proof
-        self.previous_hash = previous_hash
-
-    def __str__(self):
-        return str(self.__dict__)
-
-    def hash(self):
-        # stringify self as ordered json
-        string_object = json.dumps(self.__dict__, sort_keys=True)
-        block_string = string_object.encode()
-
-        # hash json
-        raw_hash = hashlib.sha256(block_string)
-        digest = raw_hash.hexdigest()
-
-        return digest
-
-class Transaction:
-    pass
-
 class Blockchain:
     def __init__(self):
         # list to add blocks to
@@ -44,57 +19,65 @@ class Blockchain:
         return self.chain[-1]
 
     @staticmethod
-    def hash_algorithm(prev_proof, new_proof):
+    def proof_algorithm(prev_proof, new_proof):
         return hashlib.sha256(str(new_proof ** 2 - prev_proof ** 2).encode()).hexdigest()
 
     def new_block(self, proof, previous_hash=None):
-        block = Block(len(self.chain) + 1, time(), self.pending_transactions,
-                          proof, previous_hash or self.last_block.hash())
-        # block = {
-        #     'index': len(self.chain) + 1,
-        #     'timestamp': time(),
-        #     'transactions': self.pending_transactions,
-        #     'proof': proof,
-        #     'previous_hash': previous_hash or self.hash(self.last_block)
-        # }
-
+        # create a new block 
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.pending_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.last_block)
+        }
+        # clear out the pending transactions
         self.pending_transactions = []
+        # append the new block to the chain
         self.chain.append(block)
 
         return block
 
     def new_transaction(self, sender, recipient, amount):
+        # create a transaction
         transactions = {
             'sender': sender,
             'recipient': recipient,
             'amount': amount
         }
+        # add new transaction to list of pending transactions
         self.pending_transactions.append(transactions)
-        return self.last_block.index - 1
+        return self.last_block['index'] - 1
 
     def hash(self, block):
-        string_object = json.dumps(block, sort_keys=True)
-        block_string = string_object.encode()
-
-        raw_hash = hashlib.sha256(block_string)
-        hex_hash = raw_hash.hexdigest()
-
-        return hex_hash
+        # orders the keys of the block in a predictable way
+        string_block = json.dumps(block, sort_keys=True).encode()
+        # hash the block
+        return hashlib.sha256(string_block).hexdigest()
 
     def proof_of_work(self, verbose=False):
-        previous_proof = self.last_block.proof
-        new_proof = 1
+        # need the prvious proof for our proofing algorithm
+        previous_proof = self.last_block['proof']
+        # nonce is basically the guess or arbitrary number used to find the proof
+        # nonce = number used once 
+        nonce = 1
+        # loop until a valid nonce/proof is found
         check_proof = False
         while check_proof is False:
-            proof_guess = Blockchain.hash_algorithm(previous_proof, new_proof)
+            # use our algorithm to check each nonce
+            proof_guess = Blockchain.proof_algorithm(previous_proof, nonce)
+            # print data as we search
             if verbose:
-                print(new_proof, 'matches!' if proof_guess[:4] == '0000' else 'does not match')
+                print(nonce, 'matches!' if proof_guess[:4] == '0000' else 'does not match')
+            # if the proof we guessed starts with four 0000 zeros, it is accurate
+            # otherwise keep searching for a new guess
             if proof_guess[:4] == '0000':
                check_proof = True
             else:
-                new_proof += 1
+                nonce += 1
 
-        return new_proof
+        # return the number found
+        return nonce
 
     def validate_chain(self):
         # loop over entire chain
@@ -105,19 +88,19 @@ class Blockchain:
             next_block = self.chain[i + 1]
             print(i, len(self.chain))
             # if the hashes don't line up, there is a mismatch
-            if next_block.previous_hash != prev_block.hash():
+            if next_block['previous_hash'] != self.hash(prev_block):
                 return False
 
             # check that the hashes match the algorithm
-            hash_check = Blockchain.hash_algorithm(prev_block.proof,
-                                                   next_block.proof)
+            hash_check = Blockchain.proof_algorithm(prev_block['proof'],
+                                                   next_block['proof'])
             # the hash needs to start with four zeros to be valid
             if hash_check[:4] != '0000':
                 return False
 
             i += 1
 
-        # if we make it here, all hashes have checked
+        # if we make it here, all hashes have checked and passed
         return True
 
 def main():
@@ -128,7 +111,7 @@ def main():
     t3 = bc.new_transaction('spam', 'eggs', 30)
     t4 = bc.new_transaction('bacon', 'sausage', 15)
     bc.new_block(bc.proof_of_work())
-    t5 = bc.new_transaction('eggs', 'spam', 7)
+    t5 = bc.new_transaction('ham', 'sausage', 30) 
     t6 = bc.new_transaction('bacon', 'sausage', 15)
     pprint(bc.chain)
     print(bc.validate_chain())
